4bc91838162b3e10a7b0e70643821c5b14e144c0
“Matija <matijazaric9@gmail.com>
2022-04-13 16:30:29 +0200

    Make start window and initialized

--- MiniProjekat/HCIMiniProjekat/MainWindow.xaml.cs
+++ MiniProjekat/HCIMiniProjekat/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace HCIMiniProjekat
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<String> intervalsForGDP = new List<String>();
+        private List<String> intervalsForTreasureYields = new List<String>();
+
         public MainWindow()
         {
             InitializeComponent();
+            intervalsForGDP.Add("Quarterly");
+            intervalsForGDP.Add("Annual");
+            intervalsForTreasureYields.Add("Daily");
+            intervalsForTreasureYields.Add("Weekly");
+            intervalsForTreasureYields.Add("Monthly");
+
+            List<String> dataTypes = new List<String>();
+            dataTypes.Add("GDP");
+            dataTypes.Add("Tresure Yields");
+
+            dataType.ItemsSource = dataTypes;
+            dataType.SelectedIndex = 0;
+
+            interval.ItemsSource = intervalsForGDP;
+            interval.SelectedIndex = 0;
+        }
+
+        public void DrawHandler(object sender, RoutedEventArgs e) { }
+
+        public void TableHandler(object sender, RoutedEventArgs e) { }
+
+        public void CheckIntervals(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            var ind = comboBox.SelectedIndex;
+            var selectedItem = comboBox.Items[ind];
+            switch ((string)selectedItem)
+            {
+                case "GDP":
+                    interval.ItemsSource = intervalsForGDP;
+                    interval.SelectedIndex = 0;
+                    break;
+                case "Tresure Yields":
+                    interval.ItemsSource = intervalsForTreasureYields;
+                    interval.SelectedIndex = 0;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
